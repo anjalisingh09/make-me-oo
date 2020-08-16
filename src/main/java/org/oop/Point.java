58fae56d526ab2ba5bedbae8eadb47f156fbcff5
anjalisingh09 <38291012+anjalisingh09@users.noreply.github.com>
2020-08-16 09:06:09 +0530

    Update Point.java

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -8,20 +8,25 @@ public class Point {
         return x;
     }
 
-    public void setX(double x) {
-        this.x = x;
-    }
-
     public double getY() {
         return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
     }
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
+    public static double distance(Point from, Point to)
+    {
+        double xDistance = to.getX() - from.getX();
+        double yDistance = to.getY() - from.getY();
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    }
+    public static double direction(Point from, Point to)
+    {
+        double xDistance = to.getX() - from.getX();
+        double yDistance = to.getY() - from.getY();
+        return Math.atan2(yDistance, xDistance);
+    }
 }
+
